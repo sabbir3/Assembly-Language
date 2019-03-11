aadc9b035a7318072a2dcbe52d8b90f5ce29b5e0
Sabbir Ahmed <sabbir12003@gmail.com>
2019-03-11 10:59:21 +0600

    push and pop inputed number

---
+++ array1.asm
@@ -0,0 +1,57 @@
+
+PRINT MACRO P
+    MOV AH,9
+    LEA DX,P
+    INT 21H
+ENDM
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+STR1 DB "Enter input: $"
+STR2 DB "Entered Inputs are: $"
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    PRINT STR1
+    MOV CX,0
+    
+    INPUT:
+    INC CL 
+    MOV AH,1
+    INT 21H 
+    PUSH AX
+    MOV BL,AL
+    
+    CMP BL,0DH
+    
+    JNE INPUT
+    CALL NEW_LINE
+    PRINT STR2 
+    CALL NEW_LINE
+    
+    
+    LOOPS: 
+    POP AX
+    MOV AH,2
+    MOV DL,AL
+    INT 21H
+    LOOP LOOPS
+    
+    
+    MOV AH,4CH
+    INT 21H
+ENDP
+
+
+New_Line PROC
+    MOV AH,2
+    MOV DX,10
+    INT 21H       
+    MOV AH,2		 
+    MOV DX,13
+    INT 21H
+    RET 
+    ENDP
